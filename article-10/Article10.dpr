ecbd78d66bea410a53a5668429bd5fa76907701d
delphidabbler <delphidabbler@gmail.com>
2020-06-06 05:46:49 +0100

    Remove header comments from article 10 source files

--- article-10/Article10.dpr
+++ article-10/Article10.dpr
@@ -1,17 +1,3 @@
-{
- * This source code accompanies the article "How to create and use HTML resource
- * files" which can be found at
- * http://www.delphidabbler.com/articles?article=10.
- *
- * The code is merely a proof of concept and is intended only to illustrate the
- * article. It is not designed for use in its current form in finished
- * applications. The code is provided on an "AS IS" basis, WITHOUT WARRANTY OF
- * ANY KIND, either express or implied.
- *
- * $Rev: 15 $
- * $Date: 2009-09-26 04:01:39 +0100 (Sat, 26 Sep 2009) $
-}
-
 program Article10;
 
 uses
