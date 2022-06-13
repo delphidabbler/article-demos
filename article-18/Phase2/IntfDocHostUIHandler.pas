c58acd0b1ff86390fb30a2f3ceca73e7a54b6424
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-06-13 16:23:30 +0100

    Change license from MPL/GPL/LGPL to MPL 2.0

--- article-18/Phase2/IntfDocHostUIHandler.pas
+++ article-18/Phase2/IntfDocHostUIHandler.pas
@@ -5,43 +5,11 @@
  * or automating IE to replace the menus, toolbars, and context menus. The
  * content of the unit is based on Microsoft UI documentation from MSDN.
  *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * https://www.mozilla.org/en-US/MPL/1.1/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is IntfDocHostUIHandler.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (https://delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2005-2010 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s)
- *   NONE
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK *****
+ * Copyright (C) 2005-2010 Peter Johnson. All Rights Reserved.
+ 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, 
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
 }
 
 
