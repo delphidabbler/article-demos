3084a915416a5b36e1f829dbc3c76e88962125c8
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-06-14 10:38:31 +0100

    Remove header comments from source files

--- article-12/SendToDemo.dpr
+++ article-12/SendToDemo.dpr
@@ -1,13 +1,3 @@
-(*
-  All the code presented in this unit is Copyright Peter D Johnson
-  www.delphidabbler.com, (c) 2004.
-
-  The code may be used in any way providing it is not sold or passed off as
-  the work of another person.
-
-  The code is used at the user's own risk. No guarantee or warranty is provided.
-*)
-
 program SendToDemo;
 
 uses
